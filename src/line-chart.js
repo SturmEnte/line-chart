4867409e8117f2ded3eb225d183f16ec519f4c32
SturmEnte <sturmente@proton.me>
2022-11-05 02:20:16 +0100

    Render data

--- src/line-chart.js
+++ src/line-chart.js
@@ -20,4 +20,25 @@ class LineChart {
 		parent.appendChild(svg);
 		this.svg = svg;
 	}
+
+	/**
+	 * @param {Array} data The new data
+	 * @param {Number} yMax The maximum y number
+	 */
+	updateData(data, yMax) {
+		const lineWidth = this.width / (data.length - 1);
+		const pixelPerValue = this.height / yMax;
+		console.log(lineWidth);
+		data.forEach((d, i) => {
+			if (data.length != i + 1) {
+				const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+				line.setAttribute("x1", lineWidth * i);
+				line.setAttribute("y1", this.height - d * pixelPerValue);
+				line.setAttribute("x2", lineWidth * (i + 1));
+				line.setAttribute("y2", this.height - data[i + 1] * pixelPerValue);
+				line.setAttribute("stroke", "red");
+				this.svg.appendChild(line);
+			}
+		});
+	}
 }
