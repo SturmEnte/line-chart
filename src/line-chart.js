35e158ef3d955d93ec8a07b1cd9ef5273e24c4f3
SturmEnte <sturmente@proton.me>
2022-11-05 01:59:39 +0100

    Constructor of chart

--- src/line-chart.js
+++ src/line-chart.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+class LineChart {
+	parent;
+	svg;
+	width;
+	height;
+
+	/**
+	 * @param {HTMLElement} parent The parent
+	 * @param {Number} width The width
+	 * @param {Number} height The height
+	 */
+	constructor(parent) {
+		this.parent = parent;
+		this.width = parent.clientWidth;
+		this.height = parent.clientHeight;
+		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		svg.setAttribute("width", this.width);
+		svg.setAttribute("height", this.height);
+		svg.setAttribute("viewBox", `0 0 ${this.width} ${this.height}`);
+		parent.appendChild(svg);
+		this.svg = svg;
+	}
+}
